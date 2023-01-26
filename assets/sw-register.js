1c0ad3ec4eb482aa2b317f2a1d575f243b167341
Evan Bluhm <bluhme3@gmail.com>
2023-01-25 18:18:48 -0800

    Fix comment formatting

--- assets/sw-register.js
+++ assets/sw-register.js
@@ -1,5 +1,5 @@
-// Default service worker registration script from hugo-book only works properly if site is rendered from /, which is not the case on github pages, so instead I provide my own script.
 {{- $swJS := resources.Get "sw.js" | resources.ExecuteAsTemplate "sw.js" . -}}
+// Default service worker registration script from hugo-book only works properly if site is rendered from /, which is not the case on github pages, so instead I provide my own script.
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
     "{{ $swJS.RelPermalink }}", 
