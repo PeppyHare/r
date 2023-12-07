d6944f8d20de48fc8c1be121747133729524d78d
Evan Bluhm <bluhme3@gmail.com>
2023-12-06 16:44:56 -0800

    Upgrade: Update to latest hugo-book theme version

--- assets/sw-register.js
+++
@@ -1,8 +0,0 @@
-{{- $swJS := resources.Get "sw.js" | resources.ExecuteAsTemplate "sw.js" . -}}
-// Default service worker registration script from hugo-book only works properly if site is rendered from /, which is not the case on github pages, so instead I provide my own script.
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "{{ $swJS.RelPermalink }}", 
-    { scope: "{{ "" | relURL }}" }
-  );
-}
